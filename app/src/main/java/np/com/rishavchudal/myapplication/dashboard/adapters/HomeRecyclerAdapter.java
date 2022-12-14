2822d1ba307db48bddf528dafac29f878a641b5b
Rishav Chudal <rishavchudalcitytech@Rishav-Chudals-MacBook-Pro.local>
2022-12-14 08:36:24 +0545

    Recycler view part 2 added

--- app/src/main/java/np/com/rishavchudal/myapplication/dashboard/adapters/HomeRecyclerAdapter.java
+++ app/src/main/java/np/com/rishavchudal/myapplication/dashboard/adapters/HomeRecyclerAdapter.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -47,7 +51,18 @@ public class HomeRecyclerAdapter
             @NonNull HomeRecyclerViewHolder holder,
             int position
     ) {
-
+        Product product = products.get(position);
+        holder.itemTvTitle.setText(product.title);
+        holder.itemTvDescription.setText(product.description);
+        holder.itemRootLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(
+                        context,
+                        "Item at position " + holder.getAdapterPosition() + " is clicked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -58,8 +73,33 @@ public class HomeRecyclerAdapter
     public static class HomeRecyclerViewHolder
             extends RecyclerView.ViewHolder {
 
+        private ImageView itemImageView;
+        private TextView itemTvTitle;
+        private TextView itemTvDescription;
+        private ConstraintLayout itemRootLayout;
+
         public HomeRecyclerViewHolder(@NonNull View itemView) {
             super(itemView);
+            itemImageView = itemView.findViewById(R.id.iv_item);
+            itemTvTitle = itemView.findViewById(R.id.tv_item_title);
+            itemTvDescription = itemView.findViewById(R.id.tv_item_description);
+            itemRootLayout = itemView.findViewById(R.id.item_root_layout);
+        }
+
+        public ImageView getItemImageView() {
+            return itemImageView;
+        }
+
+        public TextView getItemTvTitle() {
+            return itemTvTitle;
+        }
+
+        public TextView getItemTvDescription() {
+            return itemTvDescription;
+        }
+
+        public ConstraintLayout getItemRootLayout() {
+            return itemRootLayout;
         }
     }
 }
--- app/src/main/java/np/com/rishavchudal/myapplication/dashboard/fragments/HomeFragment.java
+++ app/src/main/java/np/com/rishavchudal/myapplication/dashboard/fragments/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -15,7 +16,12 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import np.com.rishavchudal.myapplication.R;
+import np.com.rishavchudal.myapplication.dashboard.Product;
+import np.com.rishavchudal.myapplication.dashboard.adapters.HomeRecyclerAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -70,5 +76,39 @@ public class HomeFragment extends Fragment {
                 ).show();
             }
         });
+
+        List<Product> products = getProductList();
+        HomeRecyclerAdapter recyclerAdapter = new HomeRecyclerAdapter(
+                requireActivity(),
+                products
+        );
+        recyclerView.setAdapter(recyclerAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
     }
+
+    private List<Product> getProductList() {
+        //TODO Replace with actual list from storage
+        List<Product> products = new ArrayList<>();
+
+        Product product1 = new Product();
+        product1.title = "Title 1";
+        product1.description = "Description 1";
+        products.add(product1);
+
+        Product product2 = new Product();
+        product2.title = "Title 2";
+        product2.description = "Description 2";
+        products.add(product2);
+
+        Product product3 = new Product();
+        product3.title = "Title 3";
+        product3.description = "Description 3";
+        products.add(product3);
+
+        return products;
+    }
+
+
+
 }
